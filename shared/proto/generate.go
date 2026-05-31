a0f0fa984d365f1a33e4dfd943d921b6121c1676
KingBenny101 <47111832+KingBenny101@users.noreply.github.com>
2026-05-31 23:03:26 +0530

    Remove shared proto definitions and AniDB shared models

--- shared/proto/generate.go
+++
@@ -1,5 +0,0 @@
-package proto
-
-//go:generate protoc --go_out=./metadata --go_opt=paths=source_relative --go-grpc_out=./metadata --go-grpc_opt=paths=source_relative metadata.proto
-//go:generate protoc --go_out=./indexer --go_opt=paths=source_relative --go-grpc_out=./indexer --go-grpc_opt=paths=source_relative indexer.proto
-//go:generate protoc --go_out=./downloader --go_opt=paths=source_relative --go-grpc_out=./downloader --go-grpc_opt=paths=source_relative downloader.proto
